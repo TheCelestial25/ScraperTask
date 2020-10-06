22ebfb1fe37dd05898a1b2c753a0c0d4633f2b3d
Priyanshu Singh <37958393+TheCelestial25@users.noreply.github.com>
2020-10-06 13:13:39 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// Search term
+exports.searchTerm = "sports";
+
+// Time delay in seconds
+exports.timeDelay = 30;
+
+// replace with "<your-connection-string/db-name"
+exports.connectionString = "mongodb://localhost:27017/fampay-demo"
